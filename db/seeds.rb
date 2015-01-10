1a7b0c08d4ffa944e9955350c4b4cbd3f48afd8a
Evan A <evjay.a@gmail.com>
2015-01-10 13:52:44 -0700

    added gems and seeds
    the images in the seeds file will not stay
    and there will need to be adjustments to allow for image uploads to
    show on each project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+project = Project.create(title: 'Captain America', description: 'the winter soldier', team: 'Alpha')
+project = Project.create(title: 'Hulk', description: 'the invincible soldier', team: 'Alpha')
+project = Project.create(title: 'Iron Man', description: 'Rich with smarts', team: 'Bravo')
+project = Project.create(title: 'Thor', description: 'From another world', team: 'Bravo')
+project = Project.create(title: 'The Mechanic', description: 'Fixing everything', team: 'Bravo')
+project = Project.create(title: 'Chuck Norris', description: '...', team: 'Bravo')
+project = Project.create(title: 'Craig Nielson', description: 'The very same', team: 'Alpha')
+project = Project.create(title: 'Caesar Fistulous', description: 'From the year of tomorrow', team: 'Alpha')
