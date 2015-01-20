002906a3770327d15033471f436afbf54e0ff1ba
Evan A <evjay.a@gmail.com>
2015-01-20 09:32:57 -0700

    set sidebar project in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
-  
+  before_action :set_sidebar_projects
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+    def set_sidebar_projects
+      @sidebar_projects = Project.all
+    end
+
 end
